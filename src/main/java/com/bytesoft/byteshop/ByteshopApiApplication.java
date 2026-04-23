e5fe35ae3e8440257b59d4aa8b3fd217c482d65c
Caio Filipe <caiofelipelg@hotmail.com>
2026-04-22 23:09:06 -0300

    chore: configuração inicial do projeto

---
+++ src/main/java/com/bytesoft/byteshop/ByteshopApiApplication.java
@@ -0,0 +1,13 @@
+package com.bytesoft.byteshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByteshopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ByteshopApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bytesoft/byteshop/ByteshopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bytesoft.byteshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByteshopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
